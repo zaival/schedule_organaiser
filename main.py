477902147239005d7c749428e5db2dc0c8ad0f2f
Vladimir <vladimir_zaycev_2000@mail.ru>
2022-07-27 20:26:27 +0300

    1. Parser for sites 2. DataFrames were complite 3. Save to .csv

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+headers ={
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 OPR/40.0.2308.81',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate, lzma, sdch',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4'
+}
+
+"""response = requests.get("http://ledlife.by/raspisanie-2", headers=headers).text
+
+soup = BeautifulSoup(response,"lxml")
+table = soup.find('table')
+
+data = pd.read_html(str(table))
+data = data[0]
+print(data)
+data.to_csv('SDUSHOR.csv', index=False)
+
+#---------
+
+response = requests.get("https://chizhovka-arena.by/fizkultura-i-sport/katanie-na-konkah", headers=headers).text
+
+soup = BeautifulSoup(response,"lxml")
+table = soup.find('table')
+
+data = pd.read_html(str(table))
+data = data[0]
+print(data)
+data.to_csv('CHIZHOVKA.csv', index=False)
+
+#---------
+"""
+
+response = requests.get("http://led.by/category/timetable/", headers=headers).text
+
+soup = BeautifulSoup(response,"lxml")
+content = soup.find('div', class_="postcontent")
+
+dates = re.findall(r"(\d{2}.\d{2}.\d{2})",content.text)
+dates1 =[]
+for i in dates:
+    if "-" not in i:
+        dates1.append(i)
+
+df = pd.DataFrame(dates1)
+df.to_csv('PRITYCKOGO.csv', index=False)
+
+times = re.findall(r"(\s\d{2}.\d{2}\s)",content.text)
+times1 = []
+for i in times:
+    if "-" not in i:
+        times1.append(i)
+
+df = pd.DataFrame(times1)
+df.to_csv('1.csv', index=False)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
